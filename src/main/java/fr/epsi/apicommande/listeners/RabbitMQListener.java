e1a9d03b11aeb2891763fc22a577bec6bfca9ab0
StevenD <dellarica.steven@gmail.com>
2025-02-15 11:31:00 +0100

    RabbitMQ Listener et RabbitMQConfig

---
+++ src/main/java/fr/epsi/apicommande/listeners/RabbitMQListener.java
@@ -0,0 +1,16 @@
+package fr.epsi.apicommande.listeners;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQListener {
+
+
+
+    @RabbitListener(queues = "produit_queue")
+    public void receiveMessage(String message) {
+        System.out.println("Message reçu dans API_Commande : " + message);
+
+    }
+}
