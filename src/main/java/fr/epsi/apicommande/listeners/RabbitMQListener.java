592ab3b5d9847b73f8b694da844143d99242c597
StevenD <dellarica.steven@gmail.com>
2025-02-17 19:58:15 +0100

    Status generate and assigning default Status on command

--- src/main/java/fr/epsi/apicommande/listeners/RabbitMQListener.java
+++ src/main/java/fr/epsi/apicommande/listeners/RabbitMQListener.java
@@ -1,12 +1,58 @@
 package fr.epsi.apicommande.listeners;
 
+import fr.epsi.apicommande.models.Commande;
+import fr.epsi.apicommande.models.Details;
+import fr.epsi.apicommande.models.Status;
+import fr.epsi.apicommande.repositories.StatusRepository;
+import fr.epsi.apicommande.services.CommandeService;
+import fr.epsi.apicommande.services.DetailsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class RabbitMQListener {
+
+    private final CommandeService commandeService;
+    private final DetailsService detailsService;
+    private final StatusRepository statusRepo;
+
+    public RabbitMQListener (CommandeService commandeService, DetailsService detailsService, StatusRepository statusRepo) {
+        this.commandeService = commandeService;
+        this.detailsService = detailsService;
+        this.statusRepo = statusRepo;
+    }
+
     @RabbitListener(queues = "product_info_queue")
-    public void receiveMessage(String message) {
-        System.out.println("Message reçu dans API_Commande : " + message);
+    public void receiveMessage(Map<String, Object> message) {
+        System.out.println("Coucou Kévin ! Message reçu dans API_Commande : " + message);
+
+        String produitId = (String) message.get("Id");
+        double price = Double.parseDouble(message.get("Price").toString());
+        int quantity = Integer.parseInt(message.get("Quantity").toString());
+
+        System.out.println("J'ai récupéré l'Id : " + produitId);
+        System.out.println("J'ai récupéré prix : " + price);
+        System.out.println("J'ai récupéré quantité : " + quantity);
+
+        Status defaultStatus = statusRepo.findByCurrentStatus("En attente")
+                .orElseThrow(() -> new RuntimeException("Le status 'En attente' est introuvable"));
+
+        Commande newCommande = new Commande();
+        newCommande.setStatus(defaultStatus);
+        Commande savedCommande = commandeService.createCommande(newCommande);
+
+        // Créer les détails liés à cette commande
+        Details details = new Details();
+        details.setProduitId(produitId);
+        details.setPrice(price);
+        details.setQuantity(quantity);
+        details.setCommande(savedCommande); // Associer la commande
+
+        // Sauvegarder les détails
+        detailsService.createDetails(details);
+
+        System.out.println("Re Coucou Kévin, la commande a été créée avec ID: " + savedCommande.getId());
     }
 }
